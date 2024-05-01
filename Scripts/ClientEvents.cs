631ae79703e49b77e6627465a8dd8399927e95f4
Rob G <robertgesicki@gmail.com>
2024-05-01 11:51:24 +0100

    Add root abstract ClientEvent class that other client events inherit from, to reduce code duplication

--- Scripts/ClientEvents.cs
+++ Scripts/ClientEvents.cs
@@ -2,100 +2,78 @@ using CharismaSDK.Sound;
 
 namespace CharismaSDK.Events
 {
+    public abstract class ClientEvent
+    {
+        public string conversationUuid;
+        public SpeechOptions speechConfig = null;
+
+        protected ClientEvent(string conversationUuid, SpeechOptions speechConfig = null)
+        {
+            this.conversationUuid = conversationUuid;
+            this.speechConfig = speechConfig;
+        }
+    }
+
     /// <summary>
     /// Options with which to start the interaction with Charisma.
     /// </summary>
-    public class StartEvent
+    public class StartEvent : ClientEvent
     {
-        public StartEvent(
-            string conversationUuid,
-            int? sceneIndex = null,
-            string startGraphReferenceId = null,
-            SpeechOptions speechConfig = null
-        )
-        {
-            this.conversationUuid = conversationUuid;
-            this.sceneIndex = sceneIndex;
-            this.startGraphReferenceId = startGraphReferenceId;
-            this.speechConfig = speechConfig;
-        }
-
-        /// <summary>
-        /// UUID of the conversation to start, obtained from `createConversation`.
-        /// </summary>
-        public string conversationUuid;
-
-        /// <summary>
-        /// Index of the scene to start.
-        /// </summary>
         public int? sceneIndex = null;
-
-        /// <summary>
-        /// Reference ID of the subplot to start.
-        /// </summary>
         public string startGraphReferenceId = null;
 
         /// <summary>
-        /// If `speechConfig` is provided, Charisma will return an audio clip of the character's voice alongside the text reply.
+        /// Player response to Charisma.
         /// </summary>
-        public SpeechOptions speechConfig = null;
+        /// <param name="conversationUuid">Id of the conversation to send the reply to.</param>
+        /// <param name="sceneIndex">Index of scene to start the playthrough from</param>
+        /// <param name="speechConfig">Changes the speech settings of the interaction.- Don't pass unless you want to change settings.</param>
+        /// /// <param name="startGraphReferenceId">Graph sublot to start the playthrough from</param>
+        public StartEvent(string conversationUuid, SpeechOptions speechConfig = null,  int? sceneIndex = null, string startGraphReferenceId = null) : base(conversationUuid, speechConfig)
+        {
+            this.sceneIndex = sceneIndex;
+            this.startGraphReferenceId = startGraphReferenceId;
+        }
     }
 
-    public class ReplyEvent
+    public class ReplyEvent : ClientEvent
     {
-        public string conversationUuid;
         public string text;
-        public SpeechOptions speechConfig;
 
         /// <summary>
         /// Player response to Charisma.
         /// </summary>
         /// <param name="conversationUuid">Id of the conversation to send the reply to.</param>
         /// <param name="text">Message to send</param>
-        /// <param name="speechConfig">Changes the speech settings of the interaction.
-        ///  - Don't pass unless you want to change settings.'</param>
-        public ReplyEvent(string conversationUuid, string text, SpeechOptions speechConfig = null)
+        /// <param name="speechConfig">Changes the speech settings of the interaction.- Don't pass unless you want to change settings.</param>
+        public ReplyEvent(string conversationUuid, string text, SpeechOptions speechConfig = null) : base(conversationUuid, speechConfig)
         {
-            this.conversationUuid = conversationUuid;
             this.text = text;
-            this.speechConfig = speechConfig;
         }
     }
 
-    public class TapEvent
+    public class TapEvent : ClientEvent
     {
-        public string conversationUuid;
-        public SpeechOptions speechConfig;
-
-        public TapEvent(string conversationUuid, SpeechOptions speechConfig = null)
+        public TapEvent(string conversationUuid, SpeechOptions speechConfig = null) : base(conversationUuid, speechConfig)
         {
-            this.conversationUuid = conversationUuid;
-            this.speechConfig = speechConfig;
         }
     }
 
-    public class ResumeEvent
+    public class ResumeEvent : ClientEvent
     {
         /// <summary>
         /// Options with which to resume a playthrough in Charisma.
         /// </summary>
         /// <param name="conversationUuid">Id of the conversation to resume.</param>
         /// <param name="speechConfig">To use speech, pass speech options.</param>
-        public ResumeEvent(string conversationUuid, SpeechOptions speechConfig = null)
+        public ResumeEvent(string conversationUuid, SpeechOptions speechConfig = null) : base(conversationUuid, speechConfig)
         {
-            this.conversationUuid = conversationUuid;
-            this.speechConfig = speechConfig;
         }
-
-        public string conversationUuid { get; set; }
-        public SpeechOptions speechConfig { get; }
     }
 
-    public class ActionEvent
+    public class ActionEvent : ClientEvent
     {
-        public string conversationUuid;
         public string action;
-        public SpeechOptions speechConfig;
 
         /// <summary>
         /// Action event, where the player does something non-verbal.
@@ -104,11 +82,9 @@ namespace CharismaSDK.Events
         /// <param name="action">The action to send</param>
         /// <param name="speechConfig">Changes the speech settings of the interaction.
         ///  - Don't pass unless you want to change settings.'</param>
-        public ActionEvent(string conversationUuid, string action, SpeechOptions speechConfig = null)
+        public ActionEvent(string conversationUuid, string action, SpeechOptions speechConfig = null) : base(conversationUuid, speechConfig)
         {
-            this.conversationUuid = conversationUuid;
             this.action = action;
-            this.speechConfig = speechConfig;
         }
     }
 }
