302f44c30185de90fbd6f9f799f0ec5dfb8691c7
Rob G <robertgesicki@gmail.com>
2024-05-28 17:33:28 +0100

    Migrate rest of required playthrough functionality to the base class. Also fix logging calls to use the Charisma logger

--- Scripts/PlaythroughInstanceBase.cs
+++ Scripts/PlaythroughInstanceBase.cs
@@ -1,41 +1,64 @@
 ﻿using System;
 using CharismaSDK;
+using CharismaSDK.Audio;
 using CharismaSDK.Events;
 using UnityEngine;
+using Logger = CharismaSDK.Logger;
 
 public abstract class PlaythroughInstanceBase : MonoBehaviour
 {
+    [Header("Charisma")]
+    [SerializeField]
+    [Tooltip("Playthrough parameters used to connect to Charisma backend and start a playthrough")]
+    protected CreatePlaythroughTokenParams _connectionTokenParams;
+
+    [SerializeField]
+    [Tooltip("ID of the graph to start the playthrough from.")]
+    protected string _startGraphReferenceId;
+
+    [SerializeField]
+    [Tooltip("Index of the scene to start the playthrough from.")] [Min(1)]
+    protected int _startFromScene;
+
+    [SerializeField]
+    [Tooltip("Configuration node of the Speech output.")]
+    protected SpeechOptions _speechOptions;
+
+    [SerializeField]
+    [Tooltip("Activate internal Charisma-specific logging.")]
+    protected bool _enableLogging;
+
     protected Playthrough _playthrough;
     protected string _conversationUuid;
     protected Playthrough.MessageDelegate _onMessageCallback;
 
-    [Header("Charisma")]
-    [Tooltip("Playthrough parameters - these are collected on Scene start currently.")]
-    public CreatePlaythroughTokenParams ConnectionTokenParams;
-
-    [SerializeField] [Tooltip("ID of the graph to start the playthrough from.")]
-    protected string _startGraphReferenceId;
-
-    [Tooltip("Activate internal Charisma-specific logging.")]
-    [SerializeField] protected bool _enableLogging;
-
     protected virtual void Start()
     {
-        CharismaSDK.Logger.logEnabled = _enableLogging;
+        Logger.logEnabled = _enableLogging;
     }
 
     public void LoadPlaythrough()
     {
         // We use these settings to create a play-through token.
-        StartCoroutine(CharismaAPI.CreatePlaythroughToken(ConnectionTokenParams, callback: (tokenResponse) =>
+        StartCoroutine(CharismaAPI.CreatePlaythroughToken(_connectionTokenParams, callback: (tokenResponse) =>
         {
             // Once we receive the callback with our token, we can create a new conversation.
             StartCoroutine(CharismaAPI.CreateConversation(tokenResponse.Token, callback: conversationUuid =>
             {
+                // We'll cache our conversation Id since we need this to send replies and other events to Charisma.
+                _conversationUuid = conversationUuid;
+
+                // We can now create a new charisma object and pass it our token.
+                _playthrough = new Playthrough(
+                    token: tokenResponse.Token,
+                    playthroughUuid: tokenResponse.PlaythroughUuid,
+                    _speechOptions
+                );
+
                 OnPlaythroughLoaded(tokenResponse, conversationUuid);
             }));
         }));
-     }
+    }
 
     /// <summary>
     /// Starts the playthrough.
@@ -46,58 +69,36 @@ public abstract class PlaythroughInstanceBase : MonoBehaviour
     {
         if (!IsPlaythroughLoaded())
         {
-            Debug.Log("Playthrough was not loaded. Please call LoadPlaythrough() first.");
+            Logger.Log("Playthrough was not loaded. Please call LoadPlaythrough() first.");
             return;
         }
 
         // We can now connect to Charisma. Once we receive the ready callback, we can start our play-through.
         _playthrough.Connect(() =>
         {
-            Debug.Log("Ready!");
+            Logger.Log("Playthrough ready to connect!");
+
+            // We can now subscribe to message events from charisma.
+            if (_onMessageCallback != default)
+            {
+                // We can now subscribe to message events from charisma.
+                _playthrough.OnMessage += OnMessageReceived;
+            }
 
             // In the start function, we pass the conversationId we cached earlier.
             _playthrough.Start(_conversationUuid, startGraphReferenceId: _startGraphReferenceId);
         });
-
-        // On message callback needs to be assigned.
-        if (_onMessageCallback != default)
-        {
-            // We can now subscribe to message events from charisma.
-            _playthrough.OnMessage += HandleMessage;
-        }
     }
 
     protected abstract void OnPlaythroughLoaded(CreatePlaythroughTokenResponse tokenResponse, string conversationUuid);
 
-    protected virtual void OnMessageReceived(MessageEvent message)
-    {
-        Debug.Log(message);
-
-        // If the message is a panel-node, we should operate on this data without trying to generate audio or access the text & character data of the node since panel-nodes have neither.
-        if (message.messageType == MessageType.panel)
-        {
-            Debug.Log("This is a panel node");
-        }
-        else
-        {
-            _onMessageCallback?.Invoke(message);
-        }
-
-        // If this is the end of the story, we disconnect from Charisma.
-        if (message.endStory)
-        {
-            _onStoryEnd.Invoke();
-            _playthrough.Disconnect();
-        }
-    }
+    protected abstract void OnMessageReceived(MessageEvent message);
 
     /// <summary>
-    /// Returns whether the Charisma playthrough has succesfully been loaded
+    /// Returns whether the Charisma playthrough has succesfully created
     /// </summary>
-    public bool IsPlaythroughLoaded()
+    protected bool IsPlaythroughLoaded()
     {
         return _playthrough != default;
     }
-
-
 }
